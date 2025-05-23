f25095a3ad051e8838bd4428c36652aa8d841278
Upayan M <upayanm3@gmail.com>
2025-05-23 22:06:09 +0530

    feat: replace anchor tag with Link component for navigation in GetStarted

--- app/src/components/Hero/GetStarted/GetStarted.tsx
+++ app/src/components/Hero/GetStarted/GetStarted.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import styles from "./GetStarted.module.css";
 
 const GetStarted = () => {
   return (
-    <div>
-      <a href="/auth" className={styles.btnShine}>
-        Get early access
-      </a>
-    </div>
+    <Link href="/auth" passHref legacyBehavior>
+      <a className={styles.btnShine}>Get early access</a>
+    </Link>
   );
 };
 
