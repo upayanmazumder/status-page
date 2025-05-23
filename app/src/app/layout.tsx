7d4e775856361e7e9ef8be50803d6457e11229a5
Upayan M <upayanm3@gmail.com>
2025-05-23 15:58:35 +0530

    feat: add Footer component for enhanced layout and include project and author information

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "../components/Auth/AuthProvider/AuthProvider";
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "Status Page",
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         <Header />
         <AuthProvider>{children}</AuthProvider>
+        <Footer />
       </body>
     </html>
   );
